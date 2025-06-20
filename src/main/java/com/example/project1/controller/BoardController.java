3d89d103381616279ec3f8f6bf97c877dabd4c3e
JaehongLIM2 <nu0044@naver.com>
2025-06-20 10:16:36 +0900

    controller, form입력 dto

---
+++ src/main/java/com/example/project1/controller/BoardController.java
@@ -0,0 +1,28 @@
+package com.example.project1.controller;
+
+import com.example.project1.dto.BoardForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("board")
+public class BoardController {
+
+    @GetMapping("write")
+    public String write() {
+
+        return "board/write";
+    }
+
+    @PostMapping("write")
+    public String writePost(BoardForm data) {
+        System.out.println("data = " + data);
+
+        return "board/write";
+    }
+}
