d3ac1c9f25ec58b7a8039d3b85b030080cf0ce99
JaehongLIM2 <nu0044@naver.com>
2025-06-20 11:26:10 +0900

    새 글 작성후 /board/list 리디렉션

--- src/main/java/com/example/project1/controller/BoardController.java
+++ src/main/java/com/example/project1/controller/BoardController.java
@@ -25,7 +25,7 @@ public class BoardController {
     @PostMapping("write")
     public String writePost(BoardForm data) {
         boardService.add(data);
-        return "board/write";
+        return "redirect:/board/list";
     }
 
     @GetMapping("list")
