eb0cc9f6586de5fcf8cae8966bf0bfc191da897a
JaehongLIM2 <nu0044@naver.com>
2025-06-20 09:57:52 +0900

    prj1 sql 파일 생성

---
+++ src/test/java/com/example/project1/controller/BoardController.java
@@ -0,0 +1,10 @@
+package com.example.project1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("board")
+@RequiredArgsConstructor
+public class BoardController {
+}
