ca5b58e0e6be9c2e5223e2b9e6f2de5138a8f22a
JaehongLIM2 <nu0044@naver.com>
2025-06-20 14:18:12 +0900

    paging

--- src/main/java/com/example/project1/repository/BoardRepository.java
+++ src/main/java/com/example/project1/repository/BoardRepository.java
@@ -2,6 +2,7 @@ package com.example.project1.repository;
 
 import com.example.project1.dto.BoardListInfo;
 import com.example.project1.entity.Board;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,5 +13,5 @@ public interface BoardRepository extends JpaRepository<Board, Integer> {
   // 오버로드용 파라미터 없는 것
   List<BoardListInfo> findAllBy();
 
-  List<BoardListInfo> findAllBy(Pageable pageable);
+  Page<BoardListInfo> findAllBy(Pageable pageable);
 }
